41481e864365aba8bf32967adc46fc75156c8c71
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-06-05 16:29:40 -0400

    first commit

---
+++ MEAN-DocMachine-Frontend/src/app/Pages/sign-up/sign-up.component.ts
@@ -0,0 +1,82 @@
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [CommonModule, FormsModule, NgClass, ReactiveFormsModule],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss'
+})
+export class SignUpComponent implements OnInit {
+
+
+  password = 'password';
+  password1 = 'password';
+  show = false;
+  show1 = false;
+  isDisabled: boolean = false;
+  isVisible: boolean = false;
+  submitted = false;
+  registerForm!: FormGroup;
+  closeResult!: string;
+  checked: boolean = false;
+
+
+  constructor(private formBuilder: FormBuilder, private modalService: NgbModal) {
+  }
+
+  ngOnInit(): void {
+    this.password = 'password';
+    this.registerForm = this.formBuilder.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get f() { return this.registerForm.controls; }
+  onsubmit() {
+    console.log(this.registerForm.value)
+  }
+
+
+
+  onLogin() {
+    throw new Error('Method not implemented.');
+  }
+  onClick() {
+    if (this.password === 'password') {
+      this.password = 'text';
+      this.show = true;
+    } else {
+      this.password = 'password';
+      this.show = false;
+    }
+  }
+
+  open(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: 'lg' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  onCheck(event: any) {
+    this.checked = !this.checked
+  }
+}
---
+++ MEAN-DocMachine-Frontend/src/assets/custom.js
@@ -0,0 +1,22 @@
+
+    $(document).ready(function() {
+    function addRemoveClass(theRows) {
+        theRows.removeClass("odd even");
+        theRows.filter(":odd").addClass("odd");
+        theRows.filter(":even").addClass("even");
+    }
+    var rows = $("table#myTable tr:not(:first-child)");
+    addRemoveClass(rows);
+    $("#dropdownField").on("change", function() {
+        var selected = this.value;
+        if (selected != "All") {
+            rows.filter("[position=" + selected + "]").show();
+            rows.not("[position=" + selected + "]").hide();
+            var visibleRows = rows.filter("[position=" + selected + "]");
+            addRemoveClass(visibleRows);
+        } else {
+            rows.show();
+           addRemoveClass(rows);
+        }
+    });
+});
