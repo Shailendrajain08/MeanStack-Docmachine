08df10a7c6684b48589b15b98f73dbeb35bdaa22
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-06-12 11:25:55 -0400

    Login frontend

--- MEAN-DocMachine-Frontend/src/app/AuthPages/login/login.component.ts
+++ MEAN-DocMachine-Frontend/src/app/AuthPages/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+isDisabled: any;
+inputFun(arg0: any) {
+throw new Error('Method not implemented.');
+}
+otp: any;
+open(_t67: TemplateRef<any>) {
+throw new Error('Method not implemented.');
+}
+show: any;
+onClick() {
+throw new Error('Method not implemented.');
+}
+submitted: any;
+f: any;
+password: any;
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+loginForm: FormGroup<any> | undefined;
+onSignup() {
+throw new Error('Method not implemented.');
+}
+isVisible: any;
 
 }
